e9b1b6a00177d2fc942ebfedd7f2e546f2d3d42f
SlyFoxer <57837488+SlyFoxer@users.noreply.github.com>
2021-05-30 14:47:54 -0400

    Limited number of bullets to 3 at a time

--- alien_invasion.py
+++ alien_invasion.py
@@ -66,8 +66,10 @@ class AlienInvasion:
             self.ship.moving_left = False
     
     def _fire_bullet(self):
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        """Create a new bullet and add it to the bullets group"""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
     
     
     def _update_screen(self):
