2e21e7ebee88591084c3bab73992aeadcca38c98
SlyFoxer <57837488+SlyFoxer@users.noreply.github.com>
2021-05-30 14:38:38 -0400

    Ship can now shoot bullets using spacebar

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys, pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 # Overall class for game management
 class AlienInvasion:
@@ -48,6 +49,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
     
     def _check_keyup_events(self, event):
         # Respond to key releases
@@ -56,11 +59,17 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
     
+    def _fire_bullet(self):
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+    
     
     def _update_screen(self):
         # Update images on the screen
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
 
         # Make the most recently drawn screen visible
         pygame.display.flip()
